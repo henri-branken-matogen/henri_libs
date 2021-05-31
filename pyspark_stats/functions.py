8f9757b28a13129386f02213ed25841f7eced582
Henri Branken <henri.branken@ai.matogen.com>
2021-05-31 18:20:12 +0200

    Ordered the functions in pyspark_stats.

--- pyspark_stats/functions.py
+++ pyspark_stats/functions.py
@@ -3,22 +3,78 @@ from dateutil.relativedelta import relativedelta
 from datetime import date
 
 
-def isolate(sdf_base, pkey_name, pkey_vals, *cols):
+def count_distribution(sdf_base, col_check, fancy=False):
     """
-    A very handy function that can be used for QA purposes and troubleshooting bugs.
-    :param sdf_base:  The original spark dataframe on which you want to perform filtering on.
-    :param pkey_name:  The name of the field (in string format) that you want to filter the dataframe by.
-    :param pkey_vals:  A list of value(s) that you want to retain in your filtered dataframe.
-    :param cols: All the field/column names that you want to be displayed in the filtered DataFrame.
-    :return:  Nothing is returned by this function.  However, it displays the filtered dataframe in tabular format.
+    Shows what the unique elements are for a certain column.
+    Also shows the relative contribution of each element in raw counts and percentages.
+    :param sdf_base: The base dataframe on which we want to do some inspections.
+    :param col_check:  The column whose distribution we want to investigate.
+    :param fancy:  Whether to display, or show the DataFrame.
+    :return:  Returns nothing.  Prints/Displays statistics on the screen in table format.
     """
-    ls = [pkey_name] + list(cols)
-    sdf_iso = sdf_base \
-        .filter(F.col(pkey_name).isin(pkey_vals))\
-        .select(*ls) \
-        .orderBy(pkey_name)
+    n_len = sdf_base.count()
+    sdf_base_1 = sdf_base\
+        .withColumn("idx", F.monotonically_increasing_id())
+    sdf_return = sdf_base_1\
+        .groupBy(F.col(col_check))\
+        .agg(F.count(F.col("idx")).alias("n_entries"),
+                    F.format_number(F.count(F.col("idx")) / n_len * 100.0, 5).alias("perc_entries"))\
+        .orderBy(F.col("n_entries").desc())
+    if fancy:
+        sdf_return.display(truncate=False)
+    else:
+        sdf_return.show(truncate=False)
+    return None
 
-    sdf_iso.display()
+
+def distinct_count(sdf_base, col_name):
+    """
+    Calculate the number and percentage of unique entries in `col_name`.  Returned as a tuple.
+    :param sdf_base: The base dataframe.
+    :param col_name: The column for which we want to determine the distinct number of entries.
+    :return: Return the raw number AND percentage of unique entries contained within the column.
+    """
+    n_len = sdf_base.count()
+    val_raw = sdf_base.select(F.countDistinct(col_name)).collect()[0][0]
+    val_pct = val_raw / n_len * 100.0
+    dup_raw = n_len - val_raw
+    dup_pct = dup_raw / n_len * 100
+    print("unq_raw = {:.0f} | unq_pct = {:.6f}%.".format(val_raw, val_pct))
+    print("dup_raw = {:.0f} | dup_pct = {:.6f}%.".format(dup_raw, dup_pct))
+    return None
+
+
+def distinct_row_count(sdf_base):
+    """
+    Calculate the number and percentage of unique observations.
+    :param sdf_base: The base dataframe.
+    :return: Return the raw number AND percentage of unique observations and duplicates.
+    """
+    n_len = sdf_base.count()
+
+    cnt_unq = sdf_base.distinct().count()
+    cnt_dup = n_len - cnt_unq
+    pct_unq = cnt_unq / n_len * 100.0
+    pct_dup = 100 - pct_unq
+    print(f"Unique, and duplicate raw counts are: {cnt_unq:.0f}, {cnt_dup:.0f}.")
+    print(f"Unique, and duplicate percentages are: {pct_unq:.6f}%, {pct_dup:.6f}%.")
+    return None
+
+
+def distinct_stats(sdf_base, *args):
+    """
+    Show both raw counts AND percentages of the distinct number of elements per column.
+    :param sdf_base: The base PySpark DataFrame for which we want to calculate 'distinct' statistics.
+    :param args: The column names for which we want to calculate 'distinct' statistics.
+    :return: Returns nothing.  Displays tabular results on the screen for the user.
+    """
+    n_len = sdf_base.count()
+    sdf_return = sdf_base.select(*(F.countDistinct(c).alias(c + "_CNT") for c in tuple(args)))
+    sdf_return.show()
+    sdf_return = sdf_base.select(*(F.format_number(F.countDistinct(c) / n_len * 100.0, 6)
+                                 .alias(c + "_PCT") for c in tuple(args)))
+    sdf_return.show()
+    return None
 
 
 def extract_date_tag(val, override=False, **dte_data):
@@ -58,46 +114,22 @@ def extract_date_tag(val, override=False, **dte_data):
         return None
 
 
-def count_distribution(sdf_base, col_check, fancy=False):
+def isolate(sdf_base, pkey_name, pkey_vals, *cols):
     """
-    Shows what the unique elements are for a certain column.
-    Also shows the relative contribution of each element in raw counts and percentages.
-    :param sdf_base: The base dataframe on which we want to do some inspections.
-    :param col_check:  The column whose distribution we want to investigate.
-    :param fancy:  Whether to display, or show the DataFrame.
-    :return:  Returns nothing.  Prints/Displays statistics on the screen in table format.
+    A very handy function that can be used for QA purposes and troubleshooting bugs.
+    :param sdf_base:  The original spark dataframe on which you want to perform filtering on.
+    :param pkey_name:  The name of the field (in string format) that you want to filter the dataframe by.
+    :param pkey_vals:  A list of value(s) that you want to retain in your filtered dataframe.
+    :param cols: All the field/column names that you want to be displayed in the filtered DataFrame.
+    :return:  Nothing is returned by this function.  However, it displays the filtered dataframe in tabular format.
     """
-    n_len = sdf_base.count()
-    sdf_base_1 = sdf_base\
-        .withColumn("idx", F.monotonically_increasing_id())
-    sdf_return = sdf_base_1\
-        .groupBy(F.col(col_check))\
-        .agg(F.count(F.col("idx")).alias("n_entries"),
-                    F.format_number(F.count(F.col("idx")) / n_len * 100.0, 5).alias("perc_entries"))\
-        .orderBy(F.col("n_entries").desc())
-    if fancy:
-        sdf_return.display(truncate=False)
-    else:
-        sdf_return.show(truncate=False)
-    return None
+    ls = [pkey_name] + list(cols)
+    sdf_iso = sdf_base \
+        .filter(F.col(pkey_name).isin(pkey_vals))\
+        .select(*ls) \
+        .orderBy(pkey_name)
 
-
-def show_null_stats(sdf_base, *args):
-    """
-    Give the raw count AND percentage of NULL values for the columns specified in *args.
-    :param sdf_base: Specifies the base spark dataframe.
-    :param args: Specifies the column names for which we want to calculate NULL statistics.
-    :return: Does not return anything.  Prints out results in tabular format.
-    """
-    n_len = sdf_base.count()
-    sdf_return = sdf_base\
-        .select(*(F.sum(F.col(c).isNull().cast("int"))
-                .alias(c + "_N_NULL") for c in tuple(args)))
-    for c in tuple(args):
-        sdf_return = sdf_return\
-            .withColumn(c + "_PERC", F.format_number(F.col(c + "_N_NULL") / n_len * 100.0, 5))
-    sdf_return.show()
-    return None
+    sdf_iso.display()
 
 
 def null_percs_entire(sdf_base, fancy=False):
@@ -118,51 +150,19 @@ def null_percs_entire(sdf_base, fancy=False):
     return None
 
 
-def distinct_stats(sdf_base, *args):
+def show_null_stats(sdf_base, *args):
     """
-    Show both raw counts AND percentages of the distinct number of elements per column.
-    :param sdf_base: The base PySpark DataFrame for which we want to calculate 'distinct' statistics.
-    :param args: The column names for which we want to calculate 'distinct' statistics.
-    :return: Returns nothing.  Displays tabular results on the screen for the user.
+    Give the raw count AND percentage of NULL values for the columns specified in *args.
+    :param sdf_base: Specifies the base spark dataframe.
+    :param args: Specifies the column names for which we want to calculate NULL statistics.
+    :return: Does not return anything.  Prints out results in tabular format.
     """
     n_len = sdf_base.count()
-    sdf_return = sdf_base.select(*(F.countDistinct(c).alias(c + "_CNT") for c in tuple(args)))
+    sdf_return = sdf_base\
+        .select(*(F.sum(F.col(c).isNull().cast("int"))
+                .alias(c + "_N_NULL") for c in tuple(args)))
+    for c in tuple(args):
+        sdf_return = sdf_return\
+            .withColumn(c + "_PERC", F.format_number(F.col(c + "_N_NULL") / n_len * 100.0, 5))
     sdf_return.show()
-    sdf_return = sdf_base.select(*(F.format_number(F.countDistinct(c) / n_len * 100.0, 6)
-                                 .alias(c + "_PCT") for c in tuple(args)))
-    sdf_return.show()
-    return None
-
-
-def distinct_count(sdf_base, col_name):
-    """
-    Calculate the number and percentage of unique entries in `col_name`.  Returned as a tuple.
-    :param sdf_base: The base dataframe.
-    :param col_name: The column for which we want to determine the distinct number of entries.
-    :return: Return the raw number AND percentage of unique entries contained within the column.
-    """
-    n_len = sdf_base.count()
-    val_raw = sdf_base.select(F.countDistinct(col_name)).collect()[0][0]
-    val_pct = val_raw / n_len * 100.0
-    dup_raw = n_len - val_raw
-    dup_pct = dup_raw / n_len * 100
-    print("unq_raw = {:.0f} | unq_pct = {:.6f}%.".format(val_raw, val_pct))
-    print("dup_raw = {:.0f} | dup_pct = {:.6f}%.".format(dup_raw, dup_pct))
-    return None
-
-
-def distinct_row_count(sdf_base):
-    """
-    Calculate the number and percentage of unique observations.
-    :param sdf_base: The base dataframe.
-    :return: Return the raw number AND percentage of unique observations and duplicates.
-    """
-    n_len = sdf_base.count()
-
-    cnt_unq = sdf_base.distinct().count()
-    cnt_dup = n_len - cnt_unq
-    pct_unq = cnt_unq / n_len * 100.0
-    pct_dup = 100 - pct_unq
-    print(f"Unique, and duplicate raw counts are: {cnt_unq:.0f}, {cnt_dup:.0f}.")
-    print(f"Unique, and duplicate percentages are: {pct_unq:.6f}%, {pct_dup:.6f}%.")
     return None
