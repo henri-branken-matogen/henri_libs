37a195f3733bfba90a62b8663a9e39a019f02865
Henri Branken <henri.branken@ai.matogen.com>
2021-08-16 13:54:06 +0200

    added reversed functionality to the gen_stamp_suffixes().

--- stallion/helpers.py
+++ stallion/helpers.py
@@ -20,15 +20,15 @@ def compare(value, cutoffs, label):
     return None
 
 
-def gen_stamp_suffixes(ccyymm_01, steps):
+def gen_stamp_suffixes(ccyymm_01, steps, reverse=False):
     """
     Generate and return a list of 36 ccyymm stamps.
     The first stamp in the list is the most recent, and corresponds to month 01.
     The last stamp in the list is the most historic, and corresponds to month 36.
-    :param ccyymm_01: The ccyymm stamp, in string format, that is associated with month 01.
+    :param ccyymm_01: The latest ccyymm stamp, in string format.
     :param steps: The number of stamps we must generate.  It corresponds to the length of the returned list.
     steps must be in integer format.
-    :return:  A list of 36 ccyymm stamps that are in string format.
+    :return:  A list of `steps` ccyymm stamps that are in string format.
     E.g.: ["202101", "202012", ..., "201803", "201802"].
     """
     yyyy = int(ccyymm_01[:4])
@@ -36,11 +36,18 @@ def gen_stamp_suffixes(ccyymm_01, steps):
     dte_01 = date(year=yyyy, month=mm, day=1)
     dte_00 = dte_01 + relativedelta(months=1)  # 1 month into the future with respect to `dte_01`.
     lookup_dict = dict()
-    for n_months in range(1, steps + 1):
-        dte = dte_00 - relativedelta(months=n_months)
-        stamp = dte.strftime("%Y%m")
-        lookup_dict[n_months] = stamp
-    return lookup_dict
+    if not reversal:
+        for n_months in range(1, steps + 1):
+            dte = dte_00 - relativedelta(months=n_months)
+            stamp = dte.strftime("%Y%m")
+            lookup_dict[n_months] = stamp
+        return lookup_dict
+    else:
+        for n_months in reversed(range(1, steps + 1)):
+            dte = dte_00 - relativedelta(months=n_months)
+            stamp = dte.strftime("%Y%m")
+            lookup_dict[n_months] = stamp
+        return lookup_dict
 
 
 def gen_year_month_str_pairs(yyyy_latest=9999, mm_latest=99, today_switch=True):
