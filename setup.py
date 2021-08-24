75424842808e39e0c73d3fe077b1de6fc731e79a
Henri Branken <henri.branken@ai.matogen.com>
2021-08-24 19:31:04 +0200

    Added a portfolio_health library.

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     version='0.0.1',
     author='Henri Branken',
     packages=find_packages(include=['custom_udfs', 'pyspark_stats', 'snowflake_basics', 'cvm_activity',
-                                    'stallion']),
+                                    'stallion', 'portfolio_health']),
     license='MatogenAI',
     install_requires=['DateTime==4.3', 'numpy==1.20.3', 'py4j==0.10.9', 'pyspark==3.1.1',
-                      'python-dateutil==2.8.1', 'pytz==2021.1', 'six==1.14.0',
+                      'python-dateutil==2.8.1', 'pytz==2021.1', 'seaborn==0.11.1', 'six==1.14.0',
                       'zope.interface==5.4.0']
 )
