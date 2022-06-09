8a737df9438c8bfdf34f618d212bd00528e41605
Henri Branken <henri.branken@ai.matogen.com>
2022-06-09 16:45:22 +0200

    Updated apps.py.

--- stallion/apps.py
+++ stallion/apps.py
@@ -479,6 +479,8 @@ def Match_Applications_Contracts(sdf_0):
         .withColumn("APP_Activation_Month5", f.col("nest.APP_Activation_Month5"))\
         .drop(*["nest"])
 
+    sdf_6.printSchema()
+
     ls_keep = [
         "AIL_AvgMonthsOnBook",
         "ALL_AvgMonthsOnBook",
